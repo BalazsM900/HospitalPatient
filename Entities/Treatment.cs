0c72529088a682d83797f0f39bd211779d90973e
Molnár Balázs <molnarbalazs1226@gmail.com>
2024-11-07 13:56:56 +0100

    Add project files.

---
+++ Entities/Treatment.cs
@@ -0,0 +1,14 @@
+﻿namespace HospitalPatient.Entities
+{
+    public class Treatment
+    {
+        public int Id { get; set; }
+        public int PatientId { get; set; }
+        public int DoctorId { get; set; }
+        public string TreatmentDetails { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public virtual Patient Patient { get; set; }
+        public virtual Doctor Doctor { get; set; }
+    }
+}
